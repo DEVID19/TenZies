2f50a3dacc632b34a93c470de5e7f00dfa0a958e
DEVID19 <devidbisen84@gmail.com>
2025-01-11 12:33:59 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,84 @@
-import { useState } from "react";
+/* eslint-disable react/no-unknown-property */
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Die from "./Die";
-
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
-
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
+  const gamewon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => dice.value === die[0].value);
+  useEffect(() => {
+    if (gamewon) {
+      buttonRef.current.focus();
+    }
+  }, [gamewon]);
+  
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
 
   function rollDice() {
-    setDice(generateAllNewDice());
+    if (!gamewon) {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setDice(generateAllNewDice());
+    }
   }
-  const diceElements = dice.map((num) => <Die value={num} />);
+  function hold(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
+  ));
 
   return (
     <main>
+      {gamewon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gamewon && (
+          <p>Congratulations! You won! Press New Game to start again.</p>
+        )}
+      </div>
+      <h1
+        className="font-size: 40px;
+    margin: 0;"
+      >
+        Tenzies
+      </h1>
+      <p className="font-sans font-normal mt-0 text-center">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="grid grid-rows-[auto_auto] grid-cols-5 gap-5">
         {diceElements}
       </div>
       <button
         onClick={rollDice}
+        ref={buttonRef}
         className="h-[50px] whitespace-nowrap w-auto px-[21px] py-[6px] border-none rounded-[6px] bg-[#5035FF] text-white text-[1.2rem]"
       >
-        Roll
+        {gamewon ? "New Game" : "Roll"}
       </button>
     </main>
   );
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
 
-const Die = ({ value }) => {
+const Die = ({ value, isHeld, hold }) => {
+  const styles = {
+    backgroundColor: isHeld ? "#59E391" : "white",
+  };
+
   return (
-    <button className="h-[50px] w-[50px] shadow-md rounded-[10px] border-none bg-white font-sans text-[1.75rem] font-bold cursor-pointer">
+    <button
+      className="h-[50px] w-[50px] shadow-md rounded-[10px] border-none bg-white font-sans text-[1.75rem] font-bold cursor-pointer"
+      aria-pressed={isHeld}
+      aria-label={`Die with value ${value}, 
+            ${isHeld ? "held" : "not held"}`}
+      style={styles}
+      onClick={hold()}
+    >
       {value}
     </button>
   );
